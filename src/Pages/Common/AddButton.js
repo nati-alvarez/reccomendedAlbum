9d342148a977c65c5895120354a014f36de39ab0
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-08-29 10:34:03 -0700

    removed bootstrap

--- src/Pages/Common/AddButton.js
+++ src/Pages/Common/AddButton.js
@@ -1,67 +1,67 @@
-import {faPlus} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import React, {useState} from "react";
-import {Button, Modal} from "react-bootstrap";
-import {useSelector} from "react-redux";
-import Search from "../Search/Search";
-import LoadingImage from "../../assets/loading.jpeg";
+// import {faPlus} from "@fortawesome/free-solid-svg-icons";
+// import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+// import React, {useState} from "react";
+// import {Button, Modal} from "react-bootstrap";
+// import {useSelector} from "react-redux";
+// import Search from "../Search/Search";
+// import LoadingImage from "../../assets/loading.jpeg";
 
-function AddButton({topTen}) {
-  const releaseInfo = useSelector((state) => state.releases.search);
+// function AddButton({topTen}) {
+//   const releaseInfo = useSelector((state) => state.releases.search);
 
-  const [open, setOpen] = useState(false);
+//   const [open, setOpen] = useState(false);
 
-  //modal functions
+//   //modal functions
 
-  function openModal() {
-    setOpen(true);
-  }
+//   function openModal() {
+//     setOpen(true);
+//   }
 
-  function closeModal() {
-    setOpen(false);
-  }
+//   function closeModal() {
+//     setOpen(false);
+//   }
 
-  return releaseInfo.length > 0 ? (
-    <>
-      <Button onClick={openModal} variant="outline-success" size="sm">
-        <FontAwesomeIcon icon={faPlus} />
-      </Button>
+//   return releaseInfo.length > 0 ? (
+//     <>
+//       <Button onClick={openModal} variant="outline-success" size="sm">
+//         <FontAwesomeIcon icon={faPlus} />
+//       </Button>
 
-      <Modal show={open} onHide={closeModal} animation={false}>
-        <Modal.Body>
-          <div className="searchResultsContainer">
-            <Search topTen={topTen} />
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={closeModal}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  ) : (
-    <>
-      <Button onClick={openModal} variant="outline-success" size="sm">
-        <FontAwesomeIcon icon={faPlus} />
-      </Button>
+//       <Modal show={open} onHide={closeModal} animation={false}>
+//         <Modal.Body>
+//           <div className="searchResultsContainer">
+//             <Search topTen={topTen} />
+//           </div>
+//         </Modal.Body>
+//         <Modal.Footer>
+//           <Button variant="secondary" onClick={closeModal}>
+//             Close
+//           </Button>
+//         </Modal.Footer>
+//       </Modal>
+//     </>
+//   ) : (
+//     <>
+//       <Button onClick={openModal} variant="outline-success" size="sm">
+//         <FontAwesomeIcon icon={faPlus} />
+//       </Button>
 
-      <Modal show={open} onHide={closeModal} animation={false}>
-        <Modal.Body>
-          <div className="searchResultsContainer">
-            <div className="loadingContainer">
-              <img src={LoadingImage} alt="loading" />
-            </div>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={closeModal}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
-}
+//       <Modal show={open} onHide={closeModal} animation={false}>
+//         <Modal.Body>
+//           <div className="searchResultsContainer">
+//             <div className="loadingContainer">
+//               <img src={LoadingImage} alt="loading" />
+//             </div>
+//           </div>
+//         </Modal.Body>
+//         <Modal.Footer>
+//           <Button variant="secondary" onClick={closeModal}>
+//             Close
+//           </Button>
+//         </Modal.Footer>
+//       </Modal>
+//     </>
+//   );
+// }
 
-export default AddButton;
+// export default AddButton;
--- src/Pages/Navigation/SideNavAdmin.js
+++ src/Pages/Navigation/SideNavAdmin.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {useSelector} from "react-redux";
-import AddButton from "../Common/AddButton";
+// import AddButton from "../Common/AddButton";
 
 function SideNavAdmin({navState}) {
   const navSelect = useSelector((state) => state.user);
   return (
     <div className={`navContainer ${navSelect.show ? "activeLibrary" : ""}`}>
       <div className="addButton">
-        <AddButton />
+        {/* <AddButton /> */}
       </div>
     </div>
   );
