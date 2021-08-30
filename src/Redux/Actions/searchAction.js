fc82ac1ad273b09febc65aa2b1aa1d30c8da12ea
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-08-29 18:22:48 -0700

    seeing why data is undefined

--- src/Redux/Actions/searchAction.js
+++ src/Redux/Actions/searchAction.js
@@ -1,24 +1,23 @@
 // this action handles the users search query from either the side nav or inside their dashboard
-// once the user types into the search bar it makes an api request and returns the results. 
-import { searchTitle } from '../../API/APIcall'
+// once the user types into the search bar it makes an api request and returns the results.
+import {searchTitle} from "../../API/APIcall";
 import {DataDiviner} from "../../utils/utils";
 
 export const search = (searchParams, type) => async (dispatch) => {
-    dispatch({
-      type: "SEARCH",
-      payload: {
-        loading: true,
-      },
-    });
+  dispatch({
+    type: "SEARCH",
+    payload: {
+      loading: true,
+    },
+  });
 
-    let data = await DataDiviner(searchTitle(searchParams, type));
-
-    dispatch({
-        type: "SEARCH_SUCCESS",
-        payload: {
-          all: data,
-          loading: false,
-        },
-      });
-    
-  }
+  let data = await DataDiviner(searchTitle(searchParams, type));
+  console.log(data);
+  dispatch({
+    type: "SEARCH_SUCCESS",
+    payload: {
+      all: data,
+      loading: false,
+    },
+  });
+};
