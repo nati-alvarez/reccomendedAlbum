3a83a17bf06188dd70ad832619a5bad9e0376d57
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-08-30 16:41:02 -0700

    testing moving the local storage call higher

--- src/Pages/SignIn/Interim.js
+++ src/Pages/SignIn/Interim.js
@@ -9,14 +9,14 @@ const Interim = (props) => {
     axios
       .get(`${API_BASE_URL}/identity`, {withCredentials: true})
       .then(function (response) {
+        localStorage.setItem("userID", response.data.id);
+        localStorage.setItem("username", response.data.username);
         axios
           .post(`${API_BASE_URL}/user/`, {
             idNum: response.data.id,
             name: response.data.username,
           })
           .then(function () {
-            localStorage.setItem("userID", response.data.id);
-            localStorage.setItem("username", response.data.username);
             window.location = `${client_url}/dashboard`;
           })
           .catch(function (error) {
